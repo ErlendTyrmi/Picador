f3a39d1c1b547a0f3363334e43efa5a02fa1c17b
Erlend Tyrmi <erlend@tyrmi.com>
2018-10-22 21:49:08 +0200

    streets now clickable

--- src/picador/Controller.java
+++ src/picador/Controller.java
@@ -2,10 +2,13 @@ package picador;
 
 import javafx.animation.PauseTransition;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
 import javafx.scene.text.Text;
@@ -21,11 +24,11 @@ public class Controller{
     public void rolldice(MouseEvent mouseEvent) {
         int a = (int) (Math.random() * 6 + 1);
         int b = (int) (Math.random() * 6 + 1);
+        System.out.println("dice clicked!");
         int diceShifterA = a * 100 - 100;
         int diceShifterB = b * 100 - 100;
         diceViewA.setViewport(new Rectangle2D(600, 0, 100, 170));
         diceViewB.setViewport(new Rectangle2D(600, 0, 100, 170));
-        System.out.println("dice clicked!");
         dice.play();
         waitForIt.setOnFinished(e -> {
             diceViewA.setViewport(new Rectangle2D(diceShifterA, 0, 100, 170));
@@ -36,6 +39,8 @@ public class Controller{
 
     // Set piece on board
     public void setPiece(Player player, Field field){
+        // remove monopoly-dog.png (cat/car/boat) from previous
+        // show monopoly-dog.png at field(getName) Names: zer0, one, two...twentythree
     }
 
     // Show text (cards, buy-button and other text in centre of board)
@@ -44,27 +49,33 @@ public class Controller{
     public FlowPane card;
 
     public void showText(String text){
-        message = new Text("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        message = new Text(text);
         card = new FlowPane(message);
     }
 
     // When someone clicked the button
-    public void buttonClick(){
-        // Action depends on situation buy, pay or just OK.
+    public Button mainButton;
+    public void mainButtonClick(){
+        card.setVisible(false); // The button does nothing else, all actions are default.
     }
 
     // Set account info for players (monopoly money)
-    public TextField dogMoney, catMoney, carMoney, boatMoney;
+    public Label dogMoney, catMoney, carMoney, boatMoney;
+    public HBox dogMoneyBox, catMoneyBox, carMoneyBox, boatMoneyBox;
 
     public void setMoney(String playerName, int fortune){
         if (playerName.equals("dog")){
             dogMoney.setText(Integer.toString(fortune));
+            dogMoneyBox.setOpacity(1);
         } else if (playerName.equals("cat")){
             catMoney.setText(Integer.toString(fortune));
+            catMoneyBox.setOpacity(1);
         } else if (playerName.equals("car")){
             carMoney.setText(Integer.toString(fortune));
+            carMoneyBox.setOpacity(1);
         }else if (playerName.equals("boat")){
             boatMoney.setText(Integer.toString(fortune));
+            boatMoneyBox.setOpacity(1);
         } else {
             System.out.println("Error. Player unknown.");
         }
@@ -79,5 +90,74 @@ public class Controller{
             // Place player image on field
         }
 
+    }
+
+    // The following is for implementing chance cards where the player can pick a field (street-type)!
+    private String chosenField;
+    //private StackPane one, two, four, five, seven, eight, ten, eleven, thirteen, fourteen, sixteen,
+    //seventeen, nineteen, twenty, twentytwo, twentythree;
+    public void fieldOneChosen(MouseEvent mouseEvent) {
+        System.out.println("Field one chosen!");
+        chosenField = "one";
+    }
+    public void fieldTwoChosen(MouseEvent mouseEvent) {
+        System.out.println("Field two chosen!");
+        chosenField = "two";
+    }
+    public void fieldFourChosen(MouseEvent mouseEvent) {
+        System.out.println("Field four chosen!");
+        chosenField = "four";
+    }
+    public void fieldFiveChosen(MouseEvent mouseEvent) {
+        System.out.println("Field five chosen!");
+        chosenField = "five";
+    }
+    public void fieldSevenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field seven chosen!");
+        chosenField = "seven";
+    }
+    public void fieldEightChosen(MouseEvent mouseEvent) {
+        System.out.println("Field eight chosen!");
+        chosenField = "eight";
+    }
+    public void fieldTenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field ten chosen!");
+        chosenField = "ten";
+    }
+    public void fieldElevenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field eleven chosen!");
+        chosenField = "eleven";
+    }
+    public void fieldThirteenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field thirteen chosen!");
+        chosenField = "thirteen";
+    }
+    public void fieldFourteenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field fourteen chosen!");
+        chosenField = "fourteen";
+    }
+    public void fieldSixteenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field fifteen chosen!");
+        chosenField = "fifteen";
+    }
+    public void fieldSeventeenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field seventeen chosen!");
+        chosenField = "seventeen";
+    }
+    public void fieldNineteenChosen(MouseEvent mouseEvent) {
+        System.out.println("Field nineteen chosen!");
+        chosenField = "nineteen";
+    }
+    public void fieldTwentyChosen(MouseEvent mouseEvent) {
+        System.out.println("Field twenty chosen!");
+        chosenField = "twenty";
+    }
+    public void fieldTwentytwoChosen(MouseEvent mouseEvent) {
+        System.out.println("Field twentytwo chosen!");
+        chosenField = "twentytwo";
+    }
+    public void fieldTwentythreeChosen(MouseEvent mouseEvent) {
+        System.out.println("Field twentythree chosen!");
+        chosenField = "twentythree";
     }
 }
